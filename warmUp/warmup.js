c3462527e80460c9e6167d35466d03bd8f4fdb59
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-12-21 00:42:59 +0530

    added questions list for warmup

---
+++ warmUp/warmup.js
@@ -0,0 +1,32 @@
+// Day 1 & 2
+
+/*
+1. Write a function to check if a number is prime or not?
+*/
+
+
+
+/*
+2. Wite a function to print all the even no in the given array using for and while loop i.e.
+ arr = [10, 5, 7, 0, 8, 3, 80];
+*/
+
+/*
+3.Write a function that searches for an element in an array and returns the index, if the element is not present then just return -1? Given array is 
+arr = [4, 2, 0, 10, 8, 30] and target = 10
+*/
+
+/*
+4. Write a function that returns the number of negative numbers in an array? 
+arr = [2, -9, 17, 0, 1, -10, -4, 8] and output = 3
+*/
+
+/*
+5. Write a function that returns the largest number in an array.
+ arr = [5, 0, -7, 10, 8, 17, 1] and output = 17
+*/
+
+/*
+6. Write a function that returns the smallest number in an array. 
+arr = [5, 0, -7, 10, 8, 17, 1] and output = -7
+*/
