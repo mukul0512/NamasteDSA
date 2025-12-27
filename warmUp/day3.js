267b4e0d8499318c4fe7bfd5f0afda09f4e34fa8
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-12-27 23:23:26 +0530

    handled corner cases for secondLargestNum prob

--- warmUp/day3.js
+++ warmUp/day3.js
@@ -2,6 +2,10 @@
 Question 1. Find Second Largest number in an array, given arr = [4, 9, 0, 2, 8, 7, 1]
 */
 let arr = [4, 9, 0, 2, 8, 7, 1];
+// let arr = [4]; // only one element
+// let arr = []; // empty array
+// let arr = [-4, -9, 0, -2, -8, -7, -1]; // negative
+// let arr = [4, 9, 0, 2, 8, 7, 1, 10, 10]; // if duplicate counts as once
 let firstLargest = -Infinity; // 4, 9
 let secondLargest = -Infinity; // 4, 8
 
@@ -18,12 +22,18 @@ let secondLargest = -Infinity; // 4, 8
 // console.log(res);
 
 const secondLargestNum = (arr) => {
+    if (arr.length < 2) { // in case if arr have only one element
+        return null;
+    }
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] > firstLargest) {
             secondLargest = firstLargest;
             firstLargest = arr[i];
         }
-        else if (arr[i] > secondLargest) {
+        // else if (arr[i] > secondLargest) {
+        //     secondLargest = arr[i];
+        // }
+        else if (arr[i] > secondLargest && arr[i] != firstLargest) { // in case arr have duplicates element
             secondLargest = arr[i];
         }
     }
@@ -32,3 +42,4 @@ const secondLargestNum = (arr) => {
 
 let result = secondLargestNum(arr);
 console.log(result);
+
