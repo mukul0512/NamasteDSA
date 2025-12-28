b2e88a0a4f935678e1c39788c3bf692f5c259c32
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-12-28 23:18:53 +0530

    solve pattern print prob.

---
+++ warmUp/patternPrint.js
@@ -0,0 +1,122 @@
+// Star Pattern 
+// 1. print star pattern if n = 4
+// let n = 4;
+// for(let row = 0; row < n; row++) {
+//     let star = "";
+//     for(let col = 0; col < n; col++) {
+//         star = star + "* ";
+//     }
+//     console.log(star);
+// }
+// using function
+// let n = 4;
+// const squarePattern = (n) => {
+//     for (let i = 0; i < n; i++) {
+//         let row = "";
+//         for (let j = 0; j < n; j++) {
+//             row = row + "* ";
+//         }
+//         console.log(row);
+//     }  
+// }
+// squarePattern(n);
+
+// 2. print rightTriangle pattern
+// let n = 4;
+// const rightTriangle = (n) => {
+//     for(let i = 0; i < n; i++) {
+//         let row = "";
+//         for(let j = 0; j < i + 1; j++) {
+//             row = row + "* ";
+//         }
+//         console.log(row);
+//     }
+// }
+// rightTriangle(n);
+
+//3.  print numTriangle, n = 5
+// if initialized i and j from 1
+// let n = 5;
+// const numTriangle = (n) => {
+//     for(let i = 1; i <= n; i++) {
+//         let row = "";
+//         for(let j = 1; j <= i; j++) {
+//             row = row + j + " ";
+//         }
+//         console.log(row);
+//     }
+// }
+// numTriangle(n)
+// ---------------------------------------------
+// if initialized i and j from 0 
+// let n = 5;
+// const numTriangle = (n) => {
+//     for (let i = 0; i < n; i++) {
+//         let row = "";
+//         for (let j = 0; j <= i; j++) {
+//                row = row + (j + 1) + " ";
+//         }
+//         console.log(row);
+//     }
+// }
+// numTriangle(n)
+
+// 4. printRightNumTriangle if n = 5
+// let n = 5;
+// const printNumTriangle = (n) => {
+//     for (let i = 0; i < n; i++) {
+//         let row = "";
+//         for (let j = 0; j <= i; j++) {
+//                row = row + (i + 1) + " ";
+//         }
+//         console.log(row);
+//     }
+// }
+// printNumTriangle(n)
+
+// 5. Case 1
+// printReverseRightTriangle for n = 5
+// let n = 5;
+// const printReverseRightTriangle = (n) => {
+//     let row = "";
+//     for (let i = n; i > 0; i--) {
+//         for (let j = n; j >= i; j--) {
+//             row = row + j + " ";
+//         }
+//         row = row + "\n";
+//     }
+//     return row;
+// }
+// let result = printReverseRightTriangle(n)
+// console.log(result);
+// -----------------------------------------------
+// 6. case 2
+// let n = 5;
+// const printReverseRightTriangle = (n) => {
+//     let row = "";
+//     for (let i = 0; i < n; i++) {
+//         for (let j = 0; j < n - i; j++) {
+//             row = row + (j + 1) + " ";
+//         }
+//         row = row + "\n";
+//     }
+//     return row;
+// }
+// let result = printReverseRightTriangle(n)
+// console.log(result);
+// 7. star pattern of printStarReverseRightTriangle, n = 5
+// let n = 5;
+// const printStarReverseRightTriangle = (n) => {
+//     let row = "";
+//     for (let i = 0; i < n; i++) {
+//         for (let j = 0; j < n - i; j++) {
+//             row = row + ("*") + " ";
+//         }
+//         row = row + "\n";
+//     }
+//     return row;
+// }
+// let result = printStarReverseRightTriangle(n)
+// console.log(result);
+
+// 8.
