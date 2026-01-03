5d032b1d7dd41c3a1c8fc9a52d0d00736a0b97d2
Mukul Karnwal <karnwal.mukul@gmail.com>
2026-01-03 16:25:11 +0530

    getting started day7

---
+++ warmUp/day6.js
@@ -0,0 +1,7 @@
+console.log("Time & Space Complexity");
+// T.C = It is used to measure efficiency of algorithm in terms of speed, as the input size grows.
+
+// Efficiency =
+/*
+O(1) > o(log n) > o(n) > o(n log n) > o(n^2) > O(2^n) > o(n!)
+*/
