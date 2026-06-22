bdfd31da4a6750811d8e5877886868a35a7efb33
Mukul Karnwal <karnwal.mukul@gmail.com>
2026-06-22 21:56:16 +0530

    recape warmUp module

--- dsa/Day1/day1.js
+++ dsa/Day1/day1.js
@@ -1,6 +1,13 @@
 // Day 1 
 
 /*
-1. Write a function to check if a number is prime or not?
+1. Create a function that can add 2 numbers?
 */
 
+const addTwoNumbers = (n1, n2) => {
+    let sum = n1 + n2;
+    // return sum;
+    console.log("The sum is: " + sum);
+}
+// console.log(addTwoNumbers(num1, num2));
+addTwoNumbers(5, 10);
