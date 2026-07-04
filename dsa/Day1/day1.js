62ec77211dc739303e42a30fbaaca718508d932b
Mukul Karnwal <karnwal.mukul@gmail.com>
2026-07-04 15:55:05 +0530

    added questions list for Day1

--- dsa/Day1/day1.js
+++ dsa/Day1/day1.js
@@ -1,7 +1,7 @@
 // Day 1 
 
 /*
-1. Create a function that can add 2 numbers?
+0. Create a function that can add 2 numbers?
 */
 
 const addTwoNumbers = (n1, n2) => {
@@ -11,3 +11,38 @@ const addTwoNumbers = (n1, n2) => {
 }
 // console.log(addTwoNumbers(num1, num2));
 addTwoNumbers(5, 10);
+
+/*
+1. Write a function to check if a number is prime or not?
+*/
+
+
+
+/* 
+2. Write a function to print all the even no in the given array using for and while loop i.e. arr = [10, 5, 7, 0, 8, 3, 80];
+*/
+
+
+
+/*
+3. Write a function that searches for an element in an array and returns the index, if the element is not present then just return -1? Given array is arr = [4, 2, 0, 10, 8, 30] and target = 10 
+*/
+
+
+
+/*
+4. Write a function that returns the number of negative numbers in an array? arr = [2, -9, 17, 0, 1, -10, -4, 8] and output = 3
+*/
+
+
+
+/*
+5. Write a function that returns the largest number in an array. arr = [5, 0, -7, 10, 8, 17, 1] and output = 17
+*/
+
+
+
+/*
+6. Write a function that returns the smallest number in an array. arr = [5, 0, -7, 10, 8, 17, 1] and output = -7
+*/
+
